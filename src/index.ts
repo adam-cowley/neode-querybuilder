99261bc73d8e7e98156989af621470a67c666125
Adam Cowley <github@adamcowley.co.uk>
2020-10-30 12:13:29 +0000

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import Builder from './builder/builder'
 
 export { default as WhereStatement } from './statements/where-statement'
 
-export {
-    Direction,
-    Order,
-} from './constants'
+export * from './constants'
 
 export { default as Raw } from './raw'
 
